8994233754e5ff5e377823e8ea09151c999e6221
Kozikov Bohdan <kozikovbohdan@Kozikovs-MacBook-Air.local>
2023-09-18 05:48:15 +0300

    changed settokenfunction

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,12 @@
 import axios from 'axios'
+import Cookies from 'js-cookie'
+import { TOKEN } from '../shared/constants'
 
 export const axiosClassic = axios.create({
 	baseURL: `${import.meta.env.VITE_SERVER_URL}/api`,
 	headers: {
-		'Content-Type': 'application/json'
+		'Content-Type': 'application/json',
+		Authorization: `Bearer ${Cookies.get(TOKEN)}`
 	}
 })
 
@@ -11,6 +14,6 @@ export const setAuthToken = token => {
 	if (token) {
 		axiosClassic.defaults.headers.common['Authorization'] = `Bearer ${token}`
 	} else {
-		delete axiosClassic.defaults.headers.common['Authorization']
+		return
 	}
 }
